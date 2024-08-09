0ce22959b4facf81d4a5eb28b7146b9b612fe0c0
mema <mariamsadek54@gmail.com>
2024-08-10 02:24:38 +0300

    update 3

--- lib/Features/Posts/Data/DataSource/local_data.dart
+++ lib/Features/Posts/Data/DataSource/local_data.dart
@@ -1,22 +1,37 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:dartz/dartz.dart';
 import 'package:fire/Features/Posts/Data/Models/post_model.dart';
+import 'package:fire/global/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-abstract class PostLocalDataSource{
- Future<List<PostModel>> getAllCachedPost();
-Future<Unit> cachPost( Future<List<PostModel>>postModels);
+abstract class PostLocalDataSource {
+  Future<List<PostModel>> getAllCachedPost();
+  Future<Unit> cachPost(Future<List<PostModel>> postModels);
 }
 
-class PostLocalDataSourceImpl extends PostLocalDataSource{
-  @override
-  Future<Unit> cachPost(Future<List<PostModel>> postModels) {
-    // TODO: implement cachPost
-    throw UnimplementedError();
+abstract class PostLocalDataSourceImpl extends PostLocalDataSource {
+  final SharedPreferences sharedPreferencesd;
+
+  PostLocalDataSourceImpl({required this.sharedPreferencesd});
+  Future<Unit> cachePost(Future<List<PostModel>> postModels) async {
+    List<PostModel> postModelList = await postModels;
+    List<Map<String, dynamic>> postModelToJson =
+        postModelList.map((postModel1) => postModel1.tojson()).toList();
+    sharedPreferencesd.setString("Cached Post", json.encode(postModelToJson));
+    return Future.value(unit);
   }
 
   @override
   Future<List<PostModel>> getAllCachedPost() {
-    // TODO: implement getAllCachedPost
-    throw UnimplementedError();
+    final jsonString = sharedPreferencesd.getString('Cashed Post');
+    if (jsonString != null) {
+      List jsonDecodedData = json.decode(jsonString);
+      List<PostModel> jsonToPostModel = jsonDecodedData
+          .map<PostModel>((jsonPostModel) => PostModel.fromjson(jsonPostModel))
+          .toList();
+      return Future.value(jsonToPostModel);
+    }
+    throw emptyCachExceptions;
   }
-  
 }
