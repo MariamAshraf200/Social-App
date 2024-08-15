b3fc7035a80edefe41b3e443e23fd7d42cdb25bb
mema <mariamsadek54@gmail.com>
2024-08-15 03:04:26 +0300

    update 4

--- lib/Features/Posts/Data/DataSource/remote_data.dart
+++ lib/Features/Posts/Data/DataSource/remote_data.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:fire/Features/Posts/Data/Models/post_model.dart';
 
@@ -7,22 +8,39 @@ abstract class PostsRemoteDataSource {
   Future<Unit>addPost(PostModel post);
 }
 class PostRemoTeDataSourceImpl extends PostsRemoteDataSource{
+  final FirebaseFirestore firestore;
+
+  PostRemoTeDataSourceImpl({required this.firestore});
   @override
-  Future<Unit> addPost(PostModel post) {
-    // TODO: implement dPost
-    throw UnimplementedError();
+  Future<Unit> addPost(PostModel post) async {
+    try {
+      await firestore.collection('posts').doc(post.postId).set(
+          post.tojson());
+      return unit;
+    } catch(e){
+      throw Exception('faild add post : $e ');
+    }
+
+  }
+  @override
+  Future<List<PostModel>> getAllPost() async {
+    try {
+      final querySnapshot = await firestore.collection('posts').get();
+      return querySnapshot.docs
+          .map((doc) => PostModel.fromjson(doc.data()))
+          .toList();
+    } catch (e) {
+      throw Exception('Failed to get posts: $e');
+    }
   }
 
-  @override
-  Future<List<PostModel>> getAllPost() {
-    // TODO: implement getAllPost
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Unit> deletPost(int PostId) {
-    // TODO: implement odeletPost
-    throw UnimplementedError();
+  Future<Unit> deletPost(int postId) async {
+    try {
+      await firestore.collection('posts').doc(postId.toString()).delete();
+      return unit;
+    } catch (e) {
+      throw Exception('Failed to delete post: $e');
+    }
   }
 
 }
